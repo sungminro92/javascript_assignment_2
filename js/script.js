124c413770cae32afd3f49a7ca739d31fc07495f
sungminro <sungminro92@gmail.com>
2018-09-20 12:42:14 -0400

    Changed javascript
    Added simpler functions

--- js/script.js
+++ js/script.js
@@ -1,96 +1,89 @@
 window.onload = function() {
-	working();
+    log('working');
 };
  
-function working() {
-	console.log('working');
+function log(text) {
+	console.log(text);
 }
 
-let button1 = document.getElementById('1');
-let button2 = document.getElementById('2');
-let button3 = document.getElementById('3');
-let button4 = document.getElementById('4');
-let button5 = document.getElementById('5');
-let button6 = document.getElementById('6');
-let button7 = document.getElementById('7');
-button1.addEventListener('click', createRandomNumber);
-button2.addEventListener('click', evenOrOddNumber);
-button3.addEventListener('click', loop);
-button4.addEventListener('click', whileLoop);
-button5.addEventListener('click', simpleLoop);
-button6.addEventListener('click', forLoop);
-button7.addEventListener('click', chessBoard);
-let randomInteger;
+const numberOfButtons = 7;
+let i = 1;
+const functions = [createRandomNumber, evenOrOddNumber, loop, whileLoop, simpleLoop, forLoop, chessBoard];
+while (i <= numberOfButtons) {
+    const button = document.getElementById(`${i}`);
+    button.addEventListener('click', functions[i-1]);
+    i++;
+}
+
 function createRandomNumber() {
-	let random = Math.floor(Math.random()* 101); // this returns a random number between 0 to 100.
-	number = document.getElementById('random');
-	randomInteger = random;
-	number.innerHTML = randomInteger;
+	// let random = Math.floor(Math.random()* 101); // this returns a random number between 0 to 100.
+	const randomNumber = createRandom(101);
+	const randomElement = document.getElementById('random');
+    randomElement.innerHTML = '' + randomNumber;
 }
 
 function evenOrOddNumber() {
 	let answer = document.getElementById('answer');
-	
+    const randomInteger = document.getElementById('random').innerHTML;
+    if (randomInteger === '') {
+    	return;
+	}
 	if (randomInteger % 2 === 0) {
-		answer.innerHTML = "it is an even number!"
+		answer.innerHTML = 'it is an even number!';
 	} else if (randomInteger % 2 === 1) {
-		answer.innerHTML = "it is an odd number!"
+		answer.innerHTML = 'it is an odd number!';
 	}
 }
 
 function loop() {
 	let note = document.getElementById('note');
-	note.innerHTML = "Take a look inside the console now!";
-	let i;
-	for (i = 0; i < 100; i ++) {
+	note.innerHTML = 'Take a look inside the console now!';
+	for (let i = 0; i < 100; i ++) {
 		let squared = i * i;
-		console.log(squared);
+		log(squared);
 	}
 }
 
-// function does create number 0 or 1
-function createRandom() {
-	let randomNumber = Math.floor(Math.random()*2); // create random number between 0 and 1?
-	return randomNumber;
+function createRandom(number) {
+	return Math.floor(Math.random() * number);
 }
 
 function whileLoop() {
-	textBox.innerHTML = " ";
-
+	let textBox = document.getElementById('textBox');
+	textBox.innerHTML = '';
 	let j = 1;
-
-	while (createRandom() === 0){
-		let textBox = document.getElementById('textBox');
-		let p = document.createElement("p");
-		let text = document.createTextNode("You got heads " + j + " times")
+	while (createRandom(2) === 0){
+		let p = document.createElement('p');
+		let text = document.createTextNode('You got heads ' + j + ' times');
 		p.appendChild(text);
 		textBox.appendChild(p);
 		j++;
     }
-
-		let tailP = document.createElement("p");
-        // let tails = document.getElementById('tails');
-        let tailtext = document.createTextNode("You got tails!")
-		tailP.appendChild(tailtext);
-        textBox.appendChild(tailP);
+	let tailTextP = document.createElement('p');
+	// let tails = document.getElementById('tails');
+	let tailText = document.createTextNode('You got tails!');
+    tailTextP.appendChild(tailText);
+	textBox.appendChild(tailTextP);
 }
 
 // convert this into a for(...) loop
 function simpleLoop() {
 	let number = 1;
 	while (number < 10) {
-		console.log('#' .repeat(number));
+		log('#'.repeat(number));
 		number++;
 	}
 }
 
 function forLoop() {
+//     document
+    document.getElementById('look').innerHTML = '';
 	let look = document.getElementById('look');
-	let triangle = document.createTextNode("What an awesome equilateral triangle inside console!");
+	let triangle = document.createTextNode('What an awesome equilateral triangle inside console!');
 	look.appendChild(triangle);
 	for( i = 0; i < 20; i ++) {
-		if (i%2 == 1) {
-            console.log(" " .repeat((20-i)/2) + '#'.repeat(i));
+		if (i % 2 === 1) {
+            log(' '.repeat((20-i)/2) + '#'.repeat(i));
 		}
 	}
 }
@@ -98,25 +91,25 @@ function forLoop() {
 function chessBoard() {
 
 	for ( i= 0; i < 8; i++) {
-		if (i % 2 ==0) {
-            console.log('#  '.repeat(8));
+		if (i % 2 === 0) {
+            log('#  '.repeat(8));
 		} else {
-            console.log('  #'.repeat(8));
+            log('  #'.repeat(8));
             // }
             // if (i < 5) {
-            // 	console.log(("#".repeat(5) + " ".repeat(5)).repeat(3));
+            // 	console.log(('#'.repeat(5) + ' '.repeat(5)).repeat(3));
             // }
             // else if( i > 5 && i <= 10 ) {
-            //     console.log((" ".repeat(5) + "#".repeat(5)).repeat(3));
+            //     console.log((' '.repeat(5) + '#'.repeat(5)).repeat(3));
             // }
             // else if(i > 10 && i <= 15) {
-            //     console.log(("#".repeat(5) + " ".repeat(5)).repeat(3));
+            //     console.log(('#'.repeat(5) + ' '.repeat(5)).repeat(3));
             // }
             // else if(i > 15 && i <= 20) {
-            //     console.log((" ".repeat(5) + "#".repeat(5)).repeat(3));
+            //     console.log((' '.repeat(5) + '#'.repeat(5)).repeat(3));
             // }
             // else if(i > 20 && i <= 25) {
-            //     console.log(("#".repeat(5) + " ".repeat(5)).repeat(3));
+            //     console.log(('#'.repeat(5) + ' '.repeat(5)).repeat(3));
             // }
         }
 	}
@@ -127,15 +120,15 @@ function chessBoard() {
 //         if (i % 2 == 0) {
 //         	let chessBoard = document.getElementById('chessBoard');
 //         	let paragraph = document.createElement('p');
-//         	// paragraph.innerHTML ='("#".repeat(3) + " ".repeat(3)).repeat(3)';
-//             chess1 = document.createTextNode(('#' + "...").repeat(5));
+//         	// paragraph.innerHTML ='('#'.repeat(3) + ' '.repeat(3)).repeat(3)';
+//             chess1 = document.createTextNode(('#' + '...').repeat(5));
 // 			paragraph.appendChild(chess1);
 // 			chessBoard.appendChild(paragraph);
 // 		} else {
 //             let chessBoard = document.getElementById('chessBoard');
 //             let paragraph = document.createElement('p');
-//             // paragraph.innerHTML ='("#".repeat(3) + " ".repeat(3)).repeat(3)';
-//             chess1 = document.createTextNode(('...' + "#").repeat(5));
+//             // paragraph.innerHTML ='('#'.repeat(3) + ' '.repeat(3)).repeat(3)';
+//             chess1 = document.createTextNode(('...' + '#').repeat(5));
 //             paragraph.appendChild(chess1);
 //             chessBoard.appendChild(paragraph);
 // 		}
